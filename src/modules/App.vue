086002d3012ef0609ffe23771d743178607277bb
dwayneh <contact@dwayneh.nl>
2021-04-21 13:17:27 +0200

    Add support for disabled days

--- src/modules/App.vue
+++ src/modules/App.vue
@@ -1,45 +1,51 @@
 <template>
-
   <div id="app">
     <logo></logo>
-    <calendar v-model="value"  :firstDayOfWeek="1" :disabled-days-of-week="disabled" :format="format" :clear-button="clear" :placeholder="placeholder" ></calendar>
-    <hello :show-link="true" ></hello>
+    <calendar
+      v-model="value"
+      :firstDayOfWeek="1"
+      :disabled-days="disabledDays"
+      :disabled-days-of-week="disabled"
+      :format="format"
+      :clear-button="clear"
+      :placeholder="placeholder"
+    ></calendar>
+    <hello :show-link="true"></hello>
     <div :class="'markdown'">
-      <div class="markdown-body" v-html="html">
-      </div>
+      <div class="markdown-body" v-html="html"></div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from 'components/Logo'
-import Hello from 'components/Hello'
-import Calendar from 'components/Calendar'
-import html from 'root/README.md'
+import Logo from "components/Logo";
+import Hello from "components/Hello";
+import Calendar from "components/Calendar";
+import html from "root/README.md";
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
       html: html,
       disabled: [],
       value: new Date(),
-      format: 'yyyy-MM-dd',
+      format: "yyyy-MM-dd",
       clear: true,
-      placeholder: 'placeholder is displayed when value is null or empty'
-    }
+      disabledDays: ["2021-04-22"],
+      placeholder: "placeholder is displayed when value is null or empty"
+    };
   },
   components: {
     Logo,
     Hello,
     Calendar
   }
-}
+};
 </script>
-
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -48,16 +54,15 @@ export default {
 }
 
 * {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 .markdown {
-    width: 980px;
-    margin-right: auto;
-    margin-left: auto;
-    text-align: left;
+  width: 980px;
+  margin-right: auto;
+  margin-left: auto;
+  text-align: left;
 }
 
-@import "~assets/sass/markdown.scss"
-
+@import "~assets/sass/markdown.scss";
 </style>
